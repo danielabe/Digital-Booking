d3ad73951ce752dce2f112cd6109dedcc01aa9fa
Daniela Berardi <danielaberardi@live.com.ar>
2023-01-15 19:31:37 -0300

    [FEAT] add select

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,19 +1,71 @@
-import '../styles/Search.css';
+import React, { useState } from 'react';
 import Select from "react-select";
+import '../styles/Search.css';
+import '../styles/Select.css';
 
-import { ReactComponent as IconLinkedin } from '../assets/icons/Linkedin.svg';
+import { ReactComponent as Location1 } from '../assets/icons/Location-1.svg';
+import Location2 from '../assets/icons/Location-2.svg';
+import Location3 from '../assets/icons/Location-3.svg';
 
 const Search = ({ children }) => {
     const options = [
-        { value: "blues", label: "Blues" },
-        { value: "rock", label: "Rock" },
-        { value: "jazz", label: "Jazz" },
-        { value: "orchestra", label: "Orchestra" },
+        { value: "Buenos Aires, Argentina", label: <div className='option'><img className='icon-select location3' src={Location3} height="30px" width="30px" /><img className='icon-select location2' src={Location2} height="30px" width="30px" /><div className='location'><p className='city-option'>Buenos Aires</p><span>,</span><p className='country-option'>Argentina</p></div></div> },
+        { value: "Bariloche, Argentina", label: <div className='option'><img className='icon-select location3' src={Location3} height="30px" width="30px" /><img className='icon-select location2' src={Location2} height="30px" width="30px" /><div className='location'><p className='city-option'>Bariloche</p><span>,</span><p className='country-option'>Argentina</p></div></div> },
+        { value: "Ushuaia, Argentina", label: <div className='option'><img className='icon-select location3' src={Location3} height="30px" width="30px" /><img className='icon-select location2' src={Location2} height="30px" width="30px" /><div className='location'><p className='city-option'>Ushuaia</p><span>,</span><p className='country-option'>Argentina</p></div></div> },
+        { value: "San Martín de los Andes, Argentina", label: <div className='option'><img className='icon-select location3' src={Location3} height="30px" width="30px" /><img className='icon-select location2' src={Location2} height="30px" width="30px" /><div className='location'><p className='city-option'>San Martín de los Andes</p><span>,</span><p className='country-option'>Argentina</p></div></div> },
+        { value: "San Martín de los Andes, Argentina", label: <div className='option'><img className='icon-select location3' src={Location3} height="30px" width="30px" /><img className='icon-select location2' src={Location2} height="30px" width="30px" /><div className='location'><p className='city-option'>San Martín de los Andes</p><span>,</span><p className='country-option'>Argentina</p></div></div> },
     ];
 
-    /* const placeholder = <><IconLinkedin className='icon-social-network'></IconLinkedin></>; */
-    const placeholder = '¿A dónde vamos?';
+    const customStyles = {
+        control: (defaultStyles) => ({
+            ...defaultStyles,
+            border: "none",
+            boxShadow: "none",
+            cursor: "pointer"
+        }),
+        menuList: () => ({
+            display: 'flex',
+            flexDirection: 'column',
+            position: "relative",
+            height: 'fit-content',
+            maxHeight: '196px',
+            margin: "0 5px",
+            overflowY: 'scroll',
+            scrollbar: 'smooth',
+            "::-webkit-scrollbar": {
+                display: 'none'
+            },
+            msOverflowStyle: 'none',
+            scrollbarWidth: 'none',
+        }),
+        option: (defaultStyles, state) => ({
+            ...defaultStyles,
+            position: "relative",
+            padding: "8px 5px",
+            borderBottom: '1.4px solid var(--primary-color)',
+            backgroundColor: state.isActive ? "var(--primary-color)"
+                : state.isFocused ? "#1dbeb480" : undefined,
+            cursor: "pointer"
 
+        }),
+        singleValue: (defaultStyles) => ({
+            ...defaultStyles,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            fontWeight: 500,
+            fontSize: "14px",
+            color: "var(--terciary-color)",
+        }),
+    }
+
+    const placeholder =
+        <div className='placeholder'>
+            <Location1 className='icon-select' />
+            ¿A dónde vamos?
+        </div >;
+
+    const [isClearable, setIsClearable] = useState(true);
 
     return (
         <div className="search">
@@ -23,11 +75,13 @@ const Search = ({ children }) => {
                     <Select
                         placeholder={placeholder}
                         options={options}
+                        isClearable={isClearable}
+                        styles={customStyles}
                     /* styles={colourStyles} */
                     />
                 </div>
                 <input type="text" name="" id="" />
-                <button>Buscar</button>
+                <button className='btn-search'>Buscar</button>
             </div>
         </ div>
     )
